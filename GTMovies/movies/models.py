321a075980a13cd23b384d8b7876baf38111b615
Chang <luruichang163@gmail.com>
2025-01-30 12:58:47 -0500

    Deleted all files to re-upload correct project

--- GTMovies/movies/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    release_date = models.DateField()
-    poster_url = models.URLField()
-
-    def __str__(self):
-        return self.title
