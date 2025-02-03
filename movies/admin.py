7c83e3372f4b7108f34c3409a4885ee6d98de7c9
Nakul Shah <85168948+nakulshah04@users.noreply.github.com>
2025-02-03 14:52:33 -0500

    Clearing branch

--- movies/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Movie  # Import your model
-
-admin.site.register(Movie)  # Register it
--- movies/firebase/config/moviesAPI.py
+++
@@ -1,3 +0,0 @@
-# Link to the movies API
-
-# https://api.themoviedb.org/3/movie/now_playing?api_key=f2169e05c3c7239e9f580445e0755083&language=en-US&page=1
