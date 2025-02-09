46b80760e8af12a9e50e99ab01926924f7e461f1
Nethan Nagendran <nethan.nagendran@gmail.com>
2025-02-08 22:24:33 -0500

    updated to latest version

--- movies/models.py
+++ movies/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 class Movie(models.Model):
     tmdb_id = models.IntegerField(unique=True)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     description = models.TextField()
     release_date = models.DateField()
     poster_url = models.URLField()
     vote_count = models.IntegerField(default=0)
     vote_average = models.FloatField(default=0.0)
     popularity = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(default=timezone.now)
+
     
     def __str__(self):
         return self.title
